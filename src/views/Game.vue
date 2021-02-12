beb6a326c4fb0297e0288cfc715e3c5f9fe63019
marcio samuel <marciosamuel12@outlook.com>
2021-02-12 17:43:59 -0300

    FInalização da página de game

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,17 +1,17 @@
 <template>
   <div div class="game">
-    <Display number="?"/>
+    <Display :number='displayNumber'/>
 
     <div class="keyboard">
-      <GameButton number="1"/>
-      <GameButton number="2"/>
-      <GameButton number="3"/>
-      <GameButton number="4"/>
-      <GameButton number="5"/>
-      <GameButton number="6"/>
-      <GameButton number="7"/>
-      <GameButton number="8"/>
-      <GameButton number="9"/>
+      <GameButton number="1" @click.native="validateNumber(1)"/>
+      <GameButton number="2" @click.native="validateNumber(2)"/>
+      <GameButton number="3" @click.native="validateNumber(3)"/>
+      <GameButton number="4" @click.native="validateNumber(4)"/>
+      <GameButton number="5" @click.native="validateNumber(5)"/>
+      <GameButton number="6" @click.native="validateNumber(6)"/>
+      <GameButton number="7" @click.native="validateNumber(7)"/>
+      <GameButton number="8" @click.native="validateNumber(8)"/>
+      <GameButton number="9" @click.native="validateNumber(9)"/>
     </div>
   </div>
 </template>
@@ -28,7 +28,52 @@ export default {
   data() {
     return {
       numberList: [],
+      displayNumber: null,
+      actualNumber: {},
+      userScore: 0,
     };
+  },
+  methods: {
+    getRandomNumber() {
+      this.numberList.push(Math.floor(Math.random() * 9 + 1));
+    },
+    showNumbers() {
+      let i = 1;
+      this.numberList.forEach((number) => {
+        setTimeout(() => {
+          this.displayNumber = number;
+        }, 300 * i);
+        i += 1;
+      });
+      setTimeout(() => { this.displayNumber = '?'; }, 300 * i);
+    },
+    validateNumber(n) {
+      if (n === this.actualNumber.value) {
+        this.userScore += 1;
+        this.changeActualNumber();
+      } else {
+        this.gameOver();
+      }
+    },
+    changeActualNumber() {
+      const actualIndex = this.actualNumber.index;
+      if (actualIndex === (this.numberList.length - 1)) {
+        this.startGame();
+      } else {
+        this.actualNumber = { index: actualIndex + 1, value: this.numberList[actualIndex + 1] };
+      }
+    },
+    startGame() {
+      this.getRandomNumber();
+      this.actualNumber = { index: 0, value: this.numberList[0] };
+      this.showNumbers();
+    },
+    gameOver() {
+      this.$router.push('game-over');
+    },
+  },
+  mounted() {
+    this.startGame();
   },
 };
 </script>
