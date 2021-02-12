2d50c75d5beb0a8c020c592b7cd36e6f5fb9d999
marcio samuel <marciosamuel12@outlook.com>
2021-02-12 11:12:27 -0300

    estilo base do app

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,45 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div class="container">
+    <router-view />
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@font-face {
+  font-family: "Roboto";
+  src: url("./assets/fonts/Roboto-Regular.ttf");
 }
 
-#nav {
-  padding: 30px;
+:root {
+  --background: #1c2b2d;
+  --main-blue: #1f6f8b;
+  --light-blue: #99a8b2;
+  --base: #e6d5b8;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  font-family: "Roboto", monospace;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+body {
+  background-color: var(--background);
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.container {
+  width: 360px;
+  height: 600px;
+  max-width: 54vh;
+  max-height: 90vh;
+  border: 1px solid var(--light-blue);
+  padding: 3vh;
+  color: var(--base);
 }
 </style>
