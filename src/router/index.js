7fff9b063a9892df5d6b4eb56b4dedcc26c91c7b
marcio samuel <marciosamuel12@outlook.com>
2021-02-12 02:35:58 -0300

    commit inicial

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
+import Game from '../views/Game.vue';
+import Ranking from '../views/Ranking.vue';
+import GameOver from '../views/GameOver.vue';
 
 Vue.use(VueRouter);
 
@@ -11,12 +14,19 @@ const routes = [
     component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/game',
+    name: 'Game',
+    component: Game,
+  },
+  {
+    path: '/game-over',
+    name: 'Game Over',
+    component: GameOver,
+  },
+  {
+    path: '/ranking',
+    name: 'Ranking',
+    component: Ranking,
   },
 ];
 
---
+++ src/views/GameOver.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Game Over</h1>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
