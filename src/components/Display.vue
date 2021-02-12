85e33f227f58e4fd77fa53fb81a373e50904a635
marcio samuel <marciosamuel12@outlook.com>
2021-02-12 12:19:45 -0300

    componentes de botões e tela do jogo

---
+++ src/components/Display.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="display">
+    <span>{{number}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    number: String,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.display {
+  display: flex;
+  height: 33vh;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  span {
+    display: flex;
+    height: 22.5vh;
+    width: 22.5vh;
+    border: 0.5vh solid var(--base);
+    border-radius: 1.5vh;
+    justify-content: center;
+    align-items: center;
+    font-size: 15vh;
+  }
+}
+</style>
---
+++ src/components/GameButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <button type="button">{{number}}</button>
+</template>
+
+<script>
+export default {
+  props: {
+    number: String,
+  },
+
+};
+</script>
+
+<style lang="scss" scoped>
+button {
+    border: none;
+    border-bottom: 0.5vh solid var(--main-blue);
+    border-right: 0.5vh solid var(--main-blue);
+    background-color: var(--background);
+    color: var(--light-blue);
+    font-size: 10vh;
+    outline: none;
+    cursor: pointer;
+    &:hover,
+    :active {
+      background-color: var(--main-blue);
+      color: var(--base);
+    }
+  }
+</style>
